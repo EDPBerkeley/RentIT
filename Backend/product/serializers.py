3907a49c9c6b6f4504f530faa91e91b75da47351
Erchis Patwardhan <erchispatwardhan@berkeley.edu>
2023-01-17 15:33:41 -0500

    Added product serializer

---
+++ Backend/product/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    """
+    Class that serializes the Product Model so that
+    it can be delivered in a json format
+    """
+    class Meta:
+        model = Product
+
+        fields = (
+            'description',
+            'pub_date',
+            'price'
+        )
---
+++ Readme.md
@@ -0,0 +1,16 @@
+#README
+
+##Config File
+###Host
+
+8000
+
+###Run Browser _(Yes)_ 
+
+http://127.0.0.1:8000/
+
+###Environment Variables: 
+
+PYTHONUNBUFFERED=1;
+
+DJANGO_SETTINGS_MODULE=Backend.settings
