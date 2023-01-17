8e2b288765cc533ce43ace038c2902bda34a56a1
Erchis Patwardhan <erchispatwardhan@berkeley.edu>
2023-01-17 12:14:09 -0500

    Added home page html

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'product.apps.ProductConfig',
     'django.contrib.admin',
     'django.contrib.auth',
